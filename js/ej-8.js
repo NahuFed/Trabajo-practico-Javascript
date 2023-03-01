60b0d4e0a1b4b0997b8c7a31cfe6ad9207bfc9de
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-01 00:12:15 -0300

    agregado ejercicio 8

---
+++ js/ej-8.js
@@ -0,0 +1,10 @@
+numero = parseInt(prompt('ingrese un numero y le dire si es par o impar'))
+if (isNaN(numero) === false){
+    if(numero%2 === 0){
+        document.write('el numero es par')
+    }else{
+        document.write('el numero es impar')
+    }
+} else {
+alert('no ingreso un numero')
+}
